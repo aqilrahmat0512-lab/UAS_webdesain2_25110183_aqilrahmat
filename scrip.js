adc40097b097a6314ae40b6730a5750fa62c81a8
aqilrahmat0512-lab <aqilrahmat0512@gmail.com>
2026-07-22 10:13:34 +0700

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -1,23 +1,7 @@
-function pesan(){
-    alert("Selamat Datang di UMKM Mobil Bekas!");
+function pesanSekarang() {
+    alert("Terima kasih telah memilih Mobil Auto Mall Pekanbaru.");
 }
 
-function beli(mobil){
-    alert("Terima kasih telah memilih " + mobil);
-}
-
-function kirim(){
-
-    let nama=document.getElementById("nama").value;
-    let email=document.getElementById("email").value;
-
-    if(nama=="" || email==""){
-        alert("Data belum lengkap!");
-        return false;
-    }
-
-    alert("Terima kasih "+nama+
-    ", pesan Anda berhasil dikirim.");
-
-    return true;
+function pesanMobil(namaMobil) {
+    alert("Anda memilih " + namaMobil);
 }
