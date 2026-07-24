420e298241e5b65f0b56435ef0cc45a28b821be3
aqilrahmat0512-lab <aqilrahmat0512@gmail.com>
2026-07-24 11:39:09 +0700

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,7 +1,24 @@
-function pesanSekarang() {
-    alert("Terima kasih telah memilih Mobil Auto Mall Pekanbaru.");
+function pesan(){
+    alert("Selamat datang di Showroom Mobil Bekas!");
 }
 
-function pesanMobil(namaMobil) {
-    alert("Anda memilih " + namaMobil);
+function beli(nama){
+    alert("Anda memilih " + nama);
+}
+
+function kirim(){
+
+let nama=document.getElementById("nama").value;
+
+if(nama==""){
+    alert("Nama harus diisi!");
+}
+else{
+    alert("Terima kasih " + nama + ". Pesan Anda telah terkirim.");
+}
+
+}
+function promo(namaPromo){
+    alert("Selamat!\nAnda memilih promo: " + namaPromo +
+    "\nSilakan hubungi admin untuk informasi lebih lanjut.");
 }
